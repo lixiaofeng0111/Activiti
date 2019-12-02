9603074d419d24f4241491c4473597fe90ee4c71
lixiaofeng0111 <2795317854@qq.com>
2019-12-02 11:34:44 +0800

    工作流

---
+++ src/main/java/com/test/HolidayZipDeployment.java
@@ -0,0 +1,19 @@
+package com.test;
+
+/**
+ * Title: HolidayZipDeployment.java
+ * File description:
+ *
+ * @copyright: 2019
+ * @company: tb
+ * @Author: lxf
+ * @version: 1.0
+ * @date 2019年12月02日
+ */
+public class HolidayZipDeployment {
+/**
+ * Class description:使用zip文件进行部署
+ * @Author: lxf
+ */
+
+}
