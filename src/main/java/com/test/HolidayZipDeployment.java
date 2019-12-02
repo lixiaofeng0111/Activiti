37bb3b133b85053dd818a866e570745e27008018
lixiaofeng0111 <2795317854@qq.com>
2019-12-02 12:56:59 +0800

    使用压缩文件部署业务流程

--- src/main/java/com/test/HolidayZipDeployment.java
+++ src/main/java/com/test/HolidayZipDeployment.java
@@ -1,5 +1,12 @@
 package com.test;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.RepositoryService;
+
+import java.io.InputStream;
+import java.util.zip.ZipInputStream;
+
 /**
  * Title: HolidayZipDeployment.java
  * File description:
@@ -16,4 +23,26 @@ public class HolidayZipDeployment {
  * @Author: lxf
  */
 
+    public static void main(String[] args) {
+        //1.获取ProcessEngine对象
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+
+        //2.获取RepositoryService对象
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+
+        //3.对资源文件进行解压
+        InputStream inputStream = HolidayProcessTest.class.getClassLoader().getResourceAsStream("diagram/holiday.zip");
+
+        //4.将输入流转换为压缩流
+
+        ZipInputStream zipInputStream = new ZipInputStream(inputStream);
+
+
+        //3.进行部署
+        repositoryService.createDeployment()
+                        .addZipInputStream(zipInputStream)
+                        .name("请假流程")
+                        .deploy();
+
+    }
 }
